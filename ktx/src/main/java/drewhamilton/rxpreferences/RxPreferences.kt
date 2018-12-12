9d44c05782e273854769ddbb35cc5c90e30c51d8
Drew Hamilton <drew.hamilton.0@gmail.com>
2018-12-12 17:43:22 +0100

    Improve KDocs

--- ktx/src/main/java/drewhamilton/rxpreferences/RxPreferences.kt
+++ ktx/src/main/java/drewhamilton/rxpreferences/RxPreferences.kt
@@ -1,7 +1,10 @@
 package drewhamilton.rxpreferences
 
 /**
- * Apply a series of edits to this [RxPreferences] instance and then commit them.
+ * Apply a series of edits to [this] and then commit them.
+ *
+ * Note that calls to [RxPreferences.Editor.remove] and [RxPreferences.Editor.clear] are executed first, regardless of
+ * what order they appear in [edits].
  */
 inline fun RxPreferences.edit(edits: RxPreferences.Editor.() -> RxPreferences.Editor) =
     edits.invoke(this.edit()).commit()
