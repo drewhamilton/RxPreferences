3e8bd9e347ee3ccf62739232d05c1884a1314047
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-03-26 20:21:53 +0100

    Replace Java classes in :dagger module with Kotlin

---
+++ dagger/src/main/java/drewhamilton/rxpreferences/dagger/RxPreferencesComponent.kt
@@ -0,0 +1,51 @@
+package drewhamilton.rxpreferences.dagger
+
+import android.content.SharedPreferences
+import dagger.BindsInstance
+import dagger.Component
+import drewhamilton.rxpreferences.RxPreferences
+
+/**
+ * A Dagger component that provides an [RxPreferences] instance for a given [SharedPreferences] instance.
+ */
+@Component(modules = [RxPreferencesModule::class])
+interface RxPreferencesComponent {
+
+  /**
+   * @return an [RxPreferences] wrapping the [SharedPreferences] given to the [Builder]
+   */
+  fun rxPreferences(): RxPreferences
+
+  /**
+   * A builder for this component.
+   */
+  @Component.Builder
+  interface Builder {
+
+    /**
+     * @param sharedPreferences The preferences to be wrapped by an [RxPreferences] instance.
+     * @return this builder
+     */
+    @BindsInstance
+    fun sharedPreferences(sharedPreferences: SharedPreferences): Builder
+
+    /**
+     * @return a new [RxPreferencesComponent]
+     * @throws IllegalStateException if [sharedPreferences] has not been set
+     */
+    fun build(): RxPreferencesComponent
+  }
+
+  /**
+   * Wrapper for the static [builder] method to remain callable from Java 6
+   */
+  companion object {
+
+    /**
+     * @return a concrete [Builder] instance
+     */
+    fun builder(): Builder {
+      return DaggerRxPreferencesComponent.builder()
+    }
+  }
+}
