0e136f86537e2f078a6161ef0bc17ea687b10810
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-07-06 15:56:07 +0200

    Rewrite KDocs to follow recommended style

--- ktx/src/main/java/drewhamilton/rxpreferences/RxPreferences.kt
+++ ktx/src/main/java/drewhamilton/rxpreferences/RxPreferences.kt
@@ -1,31 +1,26 @@
 package drewhamilton.rxpreferences
 
 import io.reactivex.Observable
+import io.reactivex.Single
 
 /**
- * Retrieve an enum value from the preferences that was saved with [putEnum].
- * @param key The name of the preference to retrieve.
- * @param defaultValue Value to return if this preference does not exist.
- * @return the preference value if it exists, or [defaultValue].
- * @throws [ClassCastException] if there is a preference with this key that is not
- * a string, e.g. if [putEnumByOrdinal] was used to save the enum.
- * @throws [IllegalArgumentException] if the stored string does not resolve to a valid
- * name for a value of type [E].
+ * Get a [Single] that will emit the enum value associated with [key] once. If there is no value associated with [key],
+ * [defaultValue] will be returned.
+ *
+ * Throws [ClassCastException] if there is a preference with this key that is not a string, e.g. if [putEnumByOrdinal]
+ * was used to save the enum. Throws [IllegalArgumentException] if the stored string does not resolve to a valid name
+ * for a value of type [E].
  */
 inline fun <reified E : Enum<E>> RxPreferences.getEnumOnce(key: String, defaultValue: E) =
     getStringOnce(key, defaultValue.name)
         .map { enumValueOf<E>(it) }
 
 /**
- * Observe an enum value from the preferences that was saved with [putEnum].
- * @param key The name of the preference to retrieve.
- * @param defaultValue Value to return if this preference does not exist. This value
- * is emitted if it does not exist upon subscription and if the preference is ever
- * cleared.
- * @return an [Observable] that emits the saved preference.
- * @throws [ClassCastException] if there is a preference with this key that is not
- * a string, e.g. if [putEnumByOrdinal] was used to save the enum.
- * @throws [IllegalArgumentException] if the stored string does not resolve to a valid
+ * Observe enum values associated with [key]. The returned [Observable] emits [defaultValue] if the preference does not
+ * exist upon subscription and if the preference is ever cleared.
+ *
+ * Throws [ClassCastException] if there is a preference with this key that is not a string, e.g. if [putEnumByOrdinal]
+ * was used to save the enum. Throws [IllegalArgumentException] if the stored string does not resolve to a valid
  * name for a value of type [E].
  */
 inline fun <reified E : Enum<E>> RxPreferences.getEnumStream(key: String, defaultValue: E) =
@@ -33,29 +28,24 @@ inline fun <reified E : Enum<E>> RxPreferences.getEnumStream(key: String, defaul
         .map { enumValueOf<E>(it) }!!
 
 /**
- * Retrieve an enum value from the preferences that was saved with [putEnumByOrdinal].
- * @param key The name of the preference to retrieve.
- * @param defaultValue Value to return if this preference does not exist.
- * @return the enum if its ordinal exists under the given key, or [defaultValue].
- * @throws [ClassCastException] if there is a preference with this key that is not
- * an int, e.g. if [putEnum] was used to save the enum.
- * @throws [IndexOutOfBoundsException] if the stored int does not resolve to a valid
- * ordinal for a value of type [E].
+ * Get a [Single] that will emit the enum value associated with [key] once. If there is no value associated with [key],
+ * [defaultValue] will be returned.
+ *
+ * Throws [ClassCastException] if there is a preference with this key that is not an int, e.g. if [putEnum] was used to
+ * save the enum instead of [putEnumByOrdinal]. Throws [IndexOutOfBoundsException] if the stored int does not resolve to
+ * a valid ordinal for a value of type [E].
  */
 inline fun <reified E : Enum<E>> RxPreferences.getEnumByOrdinalOnce(key: String, defaultValue: E) =
     getIntOnce(key, defaultValue.ordinal)
         .map { enumValues<E>()[it] }
 
 /**
- * Observe an enum value from the preferences that was saved with [putEnumByOrdinal].
- * @param key The name of the preference to retrieve.
- * @param defaultValue Value to return if this preference does not exist. This value is emitted if it does not exist
- * upon subscription and if the preference is ever cleared.
- * @return an [Observable] that emits the saved preference.
- * @throws [ClassCastException] if there is a preference with this key that is not
- * a string, e.g. if [putEnum] was used to save the enum.
- * @throws [IndexOutOfBoundsException] if the stored int does not resolve to a valid
- * ordinal for a value of type [E].
+ * Observe enum values associated with [key]. The returned [Observable] emits [defaultValue] if the preference does not
+ * exist upon subscription and if the preference is ever cleared.
+ *
+ * Throws [ClassCastException] if there is a preference with this key that is not an int, e.g. if [putEnum] was used to
+ * save the enum instead of [putEnumByOrdinal]. Throws [IndexOutOfBoundsException] if the stored int does not resolve to
+ * a valid ordinal for a value of type [E].
  */
 inline fun <reified E : Enum<E>> RxPreferences.getEnumByOrdinalStream(key: String, defaultValue: E) =
     getIntStream(key, defaultValue.ordinal)
@@ -71,20 +61,18 @@ inline fun RxPreferences.edit(edits: RxPreferences.Editor.() -> RxPreferences.Ed
     edits.invoke(this.edit()).commit()
 
 /**
- * Set an enum value in the preferences editor, to be written as a name string once [RxPreferences.Editor.commit] is
- * called.
- * @param key The name of the preference to modify.
- * @param value The new value for the preference.
- * @return a reference to the same [RxPreferences.Editor] object, so you can chain calls together.
+ * Set an enum [value] in the preferences editor, to be written as a name string and associated with [key] once
+ * [RxPreferences.Editor.commit] is called.
+ *
+ * Returns a reference to the same [RxPreferences.Editor] object, so you can chain calls together.
  */
 inline fun <reified E : Enum<E>> RxPreferences.Editor.putEnum(key: String, value: E) = putString(key, value.name)
 
 /**
- * Set an enum value in the preferences editor, to be written as an ordinal int once [RxPreferences.Editor.commit] is
- * called.
- * @param key The name of the preference to modify.
- * @param value The new value for the preference.
- * @return a reference to the same [RxPreferences.Editor] object, so you can chain calls together.
+ * Set an enum [value] in the preferences editor, to be written as an ordinal int and associated with [key] once
+ * [RxPreferences.Editor.commit] is called.
+ *
+ * Returns a reference to the same [RxPreferences.Editor] object, so you can chain calls together.
  */
 inline fun <reified E : Enum<E>> RxPreferences.Editor.putEnumByOrdinal(key: String, value: E) =
     putInt(key, value.ordinal)
