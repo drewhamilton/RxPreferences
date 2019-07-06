fa23919f557451c8c2598fe2c845df1d48bbf69b
Drew Hamilton <drew.hamilton.0@gmail.com>
2019-07-06 15:09:18 +0200

    Reformat :rxpreferences

--- rxpreferences/src/test/java/drewhamilton/rxpreferences/PreferenceType.java
+++ rxpreferences/src/test/java/drewhamilton/rxpreferences/PreferenceType.java
@@ -3,16 +3,16 @@ package drewhamilton.rxpreferences;
 import java.util.Set;
 
 enum PreferenceType {
-  STRING(String.class),
-  STRING_SET(Set.class),
-  INT(Integer.class),
-  LONG(Long.class),
-  FLOAT(Float.class),
-  BOOLEAN(Boolean.class);
+    STRING(String.class),
+    STRING_SET(Set.class),
+    INT(Integer.class),
+    LONG(Long.class),
+    FLOAT(Float.class),
+    BOOLEAN(Boolean.class);
 
-  final Class<?> valueClass;
+    final Class<?> valueClass;
 
-  PreferenceType(Class<?> valueClass) {
-    this.valueClass = valueClass;
-  }
+    PreferenceType(Class<?> valueClass) {
+        this.valueClass = valueClass;
+    }
 }
